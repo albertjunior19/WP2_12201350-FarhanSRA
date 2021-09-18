b1502271c515fdaaf472bf588ad667f81c7d137d
albertjunior19 <srirafaa@gmail.com>
2021-09-18 13:21:13 +0700

    first commit

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo"Nama saya Farhan Syarif
+            saya bertempat tinggal di Kayu Tinggi Cakung Timur Jakarta Timur  
+            Saya sangat suka membaca dan berolahraga";
+    }
+}
