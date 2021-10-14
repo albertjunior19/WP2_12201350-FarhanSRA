8200f94e5808614a8d95407343879c6d2b0f3a84
albertjr <90601896+albertjunior19@users.noreply.github.com>
2021-10-14 14:40:45 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <tittle>Latihan1</tittle>
+</head>
+<body>
+    Halo kawan.... yuk kita Belajar WEB PROGRAMMING...!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+</html>
